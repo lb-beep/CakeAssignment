ab438ae241bad365ef55de409373c8aa6d3144a9
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-01-16 17:06:07 -0700

    Add files via upload

---
+++ CakeAssignment/Cake/Cake/GenericClasses/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CS_133_Projects.GenericClasses
+{
+    public class ILoveCake
+    {
+
+
+        public ILoveCake()
+        {
+
+        }
+        public string Flavor { get; set; }
+        public string Type { get; set; }
+
+ }
+
+}
---
+++ CakeAssignment/Cake/Cake/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using CS_133_Projects.GenericClasses;
+
+
+namespace CS_133_Projects
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ILoveCake GenericClasses = new ILoveCake();
+
+            
+
+            List<ILoveCake> myObject = new List<ILoveCake>();
+
+            ILoveCake firstCake = new ILoveCake();
+            firstCake.Type = "Wedding";
+            firstCake.Flavor = "Vanilla";
+
+            ILoveCake secondCake = new ILoveCake();
+            secondCake.Type = "Sweet 16";
+            secondCake.Flavor = "Chocolate";
+
+            
+            
+
+            myObject.Add(firstCake);
+            myObject.Add(secondCake);
+
+
+            ILoveCake myCakeObject = myObject.First();
+            
+
+     
+
+            Console.WriteLine(myCakeObject.Type);
+            Console.WriteLine(myCakeObject.Flavor);
+
+           
+            
+         
+
+        }
+
+        
+    }
+}
